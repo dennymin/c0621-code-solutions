f36004141ada4daf417b0c1ebea50a7d2ece6355
Denny Min <denden223@gmail.com>
2021-06-28 14:56:19 -0700

    Completed js-objects exercise. - Denny Min

---
+++ javascript-objects/main.js
@@ -0,0 +1,36 @@
+var student = {
+  firstName: 'Denny',
+  lastName: 'Min',
+  age: 23
+};
+
+student.livesInIrvine = true;
+student.previousOccupation = 'customer service rep';
+console.log('value of livesInIrvine: ' + student.livesInIrvine);
+console.log('value of previousOccupation: ' + student.previousOccupation);
+
+var fullName = student.firstName + ' ' + student.lastName;
+console.log('value of fullName: ' + fullName);
+console.log('value of student: ', student);
+
+var vehicle = {
+  make: 'honda',
+  model: 'civic',
+  year: '2020'
+};
+
+vehicle.color = 'dark gray';
+vehicle.isConvertible = false;
+console.log('value of color is: ' + vehicle.color);
+console.log('value of isConvertible is: ' + vehicle.isConvertible);
+console.log('vehicle object properties and values: ', vehicle);
+
+var pet = {
+  name: 'Pepe Min',
+  type: 'American-Eskimo'
+};
+
+delete pet.name;
+delete pet.type;
+
+console.log('value of pet: ', pet);
