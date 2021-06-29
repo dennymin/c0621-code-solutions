a7c717100599e245858ccab004477b7fc50d44a4
Denny Min <denden223@gmail.com>
2021-06-29 11:58:07 -0700

    Pushing only js data model

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,87 @@
+var orderHistory = [ // i pu images as null
+  {
+    orderNumber: '114-3941689-8772232',
+    orderPlaceDate: '08042020',
+    totalPrice: 34.00,
+    shipTo: 'JS Masher',
+    additionalDeliveryInfo: [false],
+    totalItems: 1,
+    itemData: [
+      {
+        image: null,
+        productName: 'JavaScript for Impatient Programmers',
+        productTags: ['literature', 'programming', 'Rauschmayer, Dr. Axel'],
+        productPrice: 31.55,
+        deliveredOn: '08082020',
+        supportAvailable: false,
+        returnWindowCloseDate: '09072020'
+      }
+    ]
+  },
+  {
+    orderNumber: '113-9984268-1280257',
+    orderPlaceDate: '07192020',
+    totalPrice: 44.53,
+    shipTo: 'JS Masher',
+    additionalDeliveryInfo: [true, 'handed directly to a resident'],
+    totalItems: 1,
+    itemData: [
+      {
+        image: null,
+        productName: 'The Timeless Way of Building',
+        productTags: ['literature', 'Christopher Alexander'],
+        productPrice: 41.33,
+        deliveredOn: '07202020',
+        supportAvailable: false,
+        returnWindowCloseDate: '08192020'
+      }
+    ]
+  },
+  {
+    orderNumber: '114-2875557-9059409',
+    orderPlaceDate: '07042020',
+    totalPrice: 17.22,
+    shipTo: 'JS Masher',
+    additionalDeliveryInfo: [true, 'handed directly to a resident'],
+    totalItems: 1,
+    itemData: [
+      {
+        image: null,
+        productName: 'Gamecube Controller Adapter',
+        productTags: ['electronics', 'entertainment'],
+        productPrice: 15.98,
+        deliveredOn: '07072020',
+        supportAvailable: false,
+        returnWindowCloseDate: '08052020'
+      }
+    ]
+  },
+  {
+    orderNumber: '113-2883177-2648248',
+    orderPlaceDate: '07032020',
+    totalPrice: 138.93,
+    shipTo: 'JS Masher',
+    additionalDeliveryInfo: [false],
+    totalItems: 2,
+    itemData: [
+      {
+        image: null,
+        productName: 'GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)',
+        productTags: ['electronics', 'entertainment', 'nintendo'],
+        productPrice: 94.95,
+        deliveredOn: '07052020',
+        supportAvailable: true,
+        returnWindowCloseDate: '08042020'
+      },
+      {
+        image: null,
+        productName: 'The Art of SQL',
+        productTags: ['literature', 'programming', 'Stephane Faroult'],
+        productPrice: 33.99,
+        deliveredOn: '07052020',
+        supportAvailable: false,
+        returnWindowCloseDate: '08042020'
+      }
+    ]
+  }
+];
