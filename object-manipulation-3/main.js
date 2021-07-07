5661868715390d3c0eaf358c0e2132f947f25510
Denny Min <denden223@gmail.com>
2021-07-07 13:02:39 -0700

    started algorithm

--- object-manipulation-3/main.js
+++ object-manipulation-3/main.js
@@ -1,1 +1,15 @@
 console.log('Lodash is loaded:', typeof _ !== 'undefined');
+// player generation
+// there are going to be four players that I need to account for, noting name and hand
+// i could create four different profiles for them
+
+// card deck generation
+// var cards = [];
+// for (var cardIndex = 0; cardIndex < 52; cardIndex++) {
+
+// }
+
+// card shuffling
+
+// there is quite a lot of moving parts to this algorithm
+// i need to break it down into pieces if i want to have a chance at making something work
