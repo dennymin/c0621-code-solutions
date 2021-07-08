220d6d44610700aa3c6722b6c6df82e668d3ef7f
Denny Min <denden223@gmail.com>
2021-07-08 11:35:41 -0700

    Added event listeners to the individual parts

--- javascript-forms/form-events.js
+++ javascript-forms/form-events.js
@@ -18,5 +18,13 @@ var $email = document.forms[0].elements[1];
 var $message = document.forms[0].elements[2];
 
 $name.addEventListener('focus', handleFocus);
+$name.addEventListener('blur', handleBlur);
+$name.addEventListener('input', handleInput);
+
+$email.addEventListener('focus', handleFocus);
 $email.addEventListener('blur', handleBlur);
+$email.addEventListener('input', handleInput);
+
+$message.addEventListener('focus', handleFocus);
+$message.addEventListener('blur', handleBlur);
 $message.addEventListener('input', handleInput);
