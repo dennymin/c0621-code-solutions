291eaba3aba4a8d1d9cad7607a66ee696526a6ce
Denny Min <denden223@gmail.com>
2021-07-09 11:28:32 -0700

    Optimized code by adding separate function for results and try again. Also removed element from HTML and made it so that JS would generate the screen for results and trying again. Added a box shadow to the results page so that it would focus on the results.

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,38 +1,19 @@
 // if a mistake is made, the text turns red and is underlined
 // when the mistake is corrected, we move onto the next letter with normal font
 var $spanElements = document.querySelectorAll('span');
+var $documentContainer = document.querySelector('.container');
 var textString = '';
 for (var i = 0; i < $spanElements.length; i++) {
   textString += $spanElements[i].textContent;
 }
 var lineIndex = 0;
 $spanElements[lineIndex].className = 'current';
-var $statsInfo = document.querySelector('.statistics');
 var totalInputs = 0;
 
 function compare(event) {
   if (lineIndex === $spanElements.length - 1 && event.key === textString[lineIndex]) {
     $spanElements[lineIndex].className = 'correct';
-    // statistics portion
-    $statsInfo.className = 'statistics';
-    var accuracy = lineIndex / totalInputs;
-    var accuracyInfo = document.createElement('h2');
-    var accuracyInfoText = document.createTextNode('Accuracy: ' + accuracy * 100 + '%');
-    $statsInfo.appendChild(accuracyInfo.appendChild(accuracyInfoText));
-    $statsInfo.className = 'row statistics';
-    // try again portion
-    var tryAgain = document.createElement('div');
-    var tryAgainQuestion = document.createElement('p');
-    var tryAgainQuestionText = document.createTextNode('Try Again?');
-    var tryAgainForm = document.createElement('form');
-    var tryAgainButton = document.createElement('button');
-    tryAgainButton.textContent = 'Yes';
-    tryAgainQuestion.appendChild(tryAgainQuestionText);
-    tryAgain.appendChild(tryAgainQuestion);
-    tryAgainForm.appendChild(tryAgainButton);
-    tryAgain.appendChild(tryAgainForm);
-    tryAgainButton.className = 'choice-button';
-    $statsInfo.appendChild(tryAgain);
+    showResults();
     return;
   }
   if (lineIndex <= $spanElements.length) {
@@ -49,4 +30,28 @@ function compare(event) {
     }
   }
 }
+
+function showResults() {
+  var $statsInfo = document.createElement('div');
+  $statsInfo.setAttribute('class', 'statistics');
+  $documentContainer.appendChild($statsInfo);
+  var accuracy = lineIndex / totalInputs;
+  var accuracyInfo = document.createElement('h2');
+  var accuracyInfoText = document.createTextNode('Accuracy: ' + Math.floor(accuracy * 100) + '%');
+  $statsInfo.appendChild(accuracyInfo.appendChild(accuracyInfoText));
+  // try again portion
+  var tryAgain = document.createElement('div');
+  var tryAgainQuestion = document.createElement('p');
+  var tryAgainQuestionText = document.createTextNode('Try Again?');
+  var tryAgainForm = document.createElement('form');
+  var tryAgainButton = document.createElement('button');
+  tryAgainButton.textContent = 'Yes';
+  tryAgainQuestion.appendChild(tryAgainQuestionText);
+  tryAgain.appendChild(tryAgainQuestion);
+  tryAgainForm.appendChild(tryAgainButton);
+  tryAgain.appendChild(tryAgainForm);
+  tryAgainButton.className = 'choice-button';
+  $statsInfo.appendChild(tryAgain);
+}
+
 document.addEventListener('keydown', compare);
