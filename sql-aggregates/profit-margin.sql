e96f50e56f4a82961a0ddd420477a9d037ec11ab
Denny Min <denden223@gmail.com>
2021-08-18 16:33:21 -0700

    Added progress to lvl 6

--- sql-aggregates/profit-margin.sql
+++ sql-aggregates/profit-margin.sql
@@ -1,9 +1,14 @@
 select "films"."title",
-sum("payments"."amount")
+"films"."description",
+"films"."rating",
+sum("payments"."amount")"Profits",
+count("rentals"."inventoryId") as "",
+"films"."replacementCost" as "Single Replacement Cost",
+count("inventory"."filmId") as " Current Inventory",
+"films"."replacementCost" * count("inventory"."filmId") as "Total Cost"
 from "films"
 join "inventory" using ("filmId")
 join "rentals" using ("inventoryId")
 join "payments" using ("rentalId")
-group by "films"."title"
-order by sum("payments"."amount") desc
-limit 5;
+group by "films"."filmId";
+--order by sum("payments"."amount") - ("films"."replacementCost") desc;
