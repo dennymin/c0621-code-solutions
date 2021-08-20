aab7c3c9b7cd45bae0c162a3bd0a636967d6ae8f
Denny Min <denden223@gmail.com>
2021-08-20 15:41:11 -0700

    Completed array reduce exercise. Still working on bonus challenge - Denny Min

---
+++ array-reduce/reduce.js
@@ -0,0 +1,15 @@
+// function reduce(array, reducer, initialValue) {
+//   let returningVal = null;
+//   if (!initialValue) {
+//     returningVal = null;
+//   } else {
+//     returningVal = initialValue;
+//   }
+//   for (let i = 0; i < array.length; i++) {
+//     returningVal = returningVal + reducer(array[i]);
+//   }
+//   return returningVal;
+// }
+// const test = [1, 2, 3, 4];
+
+// console.log(reduce(test, ()));
