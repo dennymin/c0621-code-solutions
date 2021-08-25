cfeef6151f4042fefb5d5ecb70b2a5ef4c2c5f0a
Denny Min <denden223@gmail.com>
2021-08-25 12:38:56 -0700

    Removed the counter property from the constructor and made the class count from within the state. Did this by adding a variable clicked in the handleClick method. - Denny Min

--- react-hot-button-component/src/hot-button.jsx
+++ react-hot-button-component/src/hot-button.jsx
@@ -4,27 +4,28 @@ export default class HotButton extends React.Component {
   constructor(props) {
     super(props);
     this.handleClick = this.handleClick.bind(this);
-    this.counter = 0;
-    this.state = { clickCount: this.counter };
+    this.state = { clickCount: 0 };
   }
 
   handleClick() {
-    this.setState({ clickCount: this.counter++ });
+    const clicked = this.state.clickCount + 1;
+    this.setState({ clickCount: clicked });
   }
 
   render() {
     let clickColor = null;
-    if (this.counter < 3) {
+    const counter = this.state.clickCount;
+    if (counter < 3) {
       clickColor = 'dark-background white-text';
-    } else if (this.counter >= 3 && this.counter < 6) {
+    } else if (counter >= 3 && counter < 6) {
       clickColor = 'purple-background white-text';
-    } else if (this.counter >= 6 && this.counter < 9) {
+    } else if (counter >= 6 && counter < 9) {
       clickColor = 'red-background white-text';
-    } else if (this.counter >= 9 && this.counter < 12) {
+    } else if (counter >= 9 && counter < 12) {
       clickColor = 'orange-background';
-    } else if (this.counter >= 12 && this.counter < 15) {
+    } else if (counter >= 12 && counter < 15) {
       clickColor = 'yellow-background';
-    } else if (this.counter >= 15) {
+    } else if (counter >= 15) {
       clickColor = 'white-background';
     }
     return <button className={clickColor} onClick={this.handleClick}>Hot Button</button>;
