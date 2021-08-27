e136d90352de67a8cb52b9799b37b18f6210dd7c
Denny Min <denden223@gmail.com>
2021-08-27 11:41:58 -0700

    Completed react accordion component exercise. - Denny Min

--- react-accordion-component/src/accordion.jsx
+++ react-accordion-component/src/accordion.jsx
@@ -3,57 +3,65 @@ export default class Accordion extends React.Component {
   constructor(props) {
     super(props);
     this.handleClick = this.handleClick.bind(this);
-    this.generateList = this.GenerateList.bind(this);
+    this.generateList = this.generateList.bind(this);
+    // this.generateIndex = this.generateIndex.bind(this);
     this.state = {
-      reveal: true
+      reveal: false,
+      focused: null
     };
     this.information = this.props.topics;
-    // this.information = [
-    //   { number: 1, name: 'Hypertext Markup Language', description: 'test' },
-    //   { number: 2, name: 'Cascading Style Sheets', description: 'test' },
-    //   { number: 3, name: 'JavaScript', description: 'test' }
-    // ];
   }
 
   handleClick(event) {
-    console.log(this.props);
-    console.log(this.props.topics);
-    console.log(this.information);
+    const difTopics = event.target.parentElement.parentElement.children;
+    if (event.target.className.includes('info-title')) {
+      this.setState({ reveal: true, focused: event.target.getAttribute('data-index') });
+      for (let i = 0; i < difTopics.length; i++) {
+        if (difTopics[i].getAttribute('data-index') === event.target.getAttribute('data-index')) {
+          difTopics[i].children[1].className = 'info-body';
+        } else {
+          difTopics[i].children[1].className = 'info-body hidden';
+        }
+      }
+    }
+    if (this.state.reveal === true && this.state.focused === event.target.getAttribute('data-index')) {
+      event.target.nextSibling.className = 'info-body hidden';
+      this.setState({ reveal: false, focused: null });
+    }
+    // else if (this.state.reveal === true && this.state.focused !== null) {
+    //   event.target.nextSibling.className = 'info-body hidden';
+    //   this.setState({ reveal: false });
+    // }
   }
 
-  GenerateList(props) {
+  generateList(props) {
     const topics = this.information;
     const topicsList = topics.map(topic =>
-      <div key={topic.number}>
-        <h3 className='info-title'>{topic.name}</h3>
-        <div>{topic.description}</div>
+      <div key={topic.number} data-index={topic.number}>
+        <h3 className='info-title' onClick={this.handleClick} data-index={topic.number}>{topic.name}</h3>
+        <div className='info-body hidden'>{topic.description}</div>
       </div>
     );
     return (
-      <div>{topicsList}</div>
+      topicsList
     );
   }
 
+  // generateIndex() {
+  //   const indexes = [];
+  //   for (let i = 0; i < this.information.length; i++) {
+  //     const entry = {
+  //       index: this.information.number,
+  //       reveal: false
+  //     };
+  //     indexes.push(entry);
+  //   }
+  //   this.setState({ arr: indexes });
+  // }
+
   render() {
     return (
-    // <div className='page flex-wrap'>
-    //   <div className='info-container flex-wrap flex-basis-100'>
-    //     <h3 onClick={this.handleClick} className='info-title flex-basis-100'>Hypertext Markup Language</h3>
-    //     <div className='info-body flex-basis-100'>Text</div>
-    //   </div>
-
-    //   <div className='info-container flex-wrap flex-basis-100'>
-    //     <h3 onClick={this.handleClick} className='info-title flex-basis-100'>Cascading Style Sheets</h3>
-    //     <div className='info-body flex-basis-100'>Text</div>
-    //   </div>
-
-    //     <div className='info-container flex-wrap flex-basis-100'>
-    //       <h3 onClick={this.handleClick} className='info-title flex-basis-100'>JavaScript</h3>
-    //       <div className='info-body flex-basis-100'>Text</div>
-    //     </div>
-    //   </div>
-      // < this.GenerateList/>
-      <h1 onClick={this.handleClick}>hi</h1>
+      < this.generateList></this.generateList>
     );
   }
 }
