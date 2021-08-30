c7116bc88f4b41c1334a841244a76ad5b6977b46
Denny Min <denden223@gmail.com>
2021-08-30 13:04:56 -0700

    Completed fetch-in-react exercise. -Denny Min

---
+++ fetch-in-react/src/app.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import UserList from './user-list';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: [],
+      isLoading: true
+    };
+  }
+
+  componentDidMount() {
+    this.setState({ isLoading: false });
+    fetch('https://jsonplaceholder.typicode.com/users', {
+      method: 'GET'
+    })
+      .then(res => res.json())
+      .then(data =>
+        this.setState({ users: data })
+      );
+  }
+
+  render() {
+    return this.state.isLoading
+      ? <p>Loading...</p>
+      : <UserList users={this.state.users} />;
+  }
+}
+
+export default App;
